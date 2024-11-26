c7024263a48c681d5409c98ab7687565ba1401a6
Moreno Ignacio <ignamoreno1@gmail.com>
2024-11-26 19:44:18 -0300

    Add files via upload

---
+++ XAcademy/Third-Test/modificarTags.cy.js
@@ -0,0 +1,8 @@
+describe('prueba',()=>{
+    it('capturar Tags', ()=>{
+    cy.intercept('GET', '/api/tags',{fixture:'tags.json'})
+       cy.visit('https://conduit.bondaracademy.com') 
+       cy.contains('Polo')
+    })
+    
+})
